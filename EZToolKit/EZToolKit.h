d4afd89c47af44d98b4eab643cac7a8af80ed27b
yangjun zhu <gzhuyangjun@gmail.com>
2015-08-20 15:26:22 +0800

    import .h file

--- EZToolKit/EZToolKit.h
+++ EZToolKit/EZToolKit.h
@@ -10,24 +10,31 @@
 
 
 #import <UIKit/UIKit.h>
-#import <EZToolKit/NSObject+EZ_Parse.hh>
+
+#import <EZToolKit/NSBundle+EZ_AppIcon.h>
+#import <EZToolKit/NSObject+EZ_Helper.h>
+#import <EZToolKit/NSObject+EZ_Parse.h>
 #import <EZToolKit/NSObject+EZ_Association.h>
 #import <EZToolKit/NSNull+EZ_System.h>
 #import <EZToolKit/NSString+EZ_Helper.h>
 #import <EZToolKit/NSString+EZ_Parse.h>
 #import <EZToolKit/NSString+EZ_Format.h>
+#import <EZToolKit/NSString+EZ_Rect.h>
 #import <EZToolKit/NSData+EZ_Parse.h>
 #import <EZToolKit/NSDate+EZ_Format.h>
 #import <EZToolKit/NSTimer+EZ_Helper.h>
+#import <EZToolKit/NSURL+EZ_Helper.h>
+
 #import <EZToolKit/UIDevice+EZ_Helper.h>
 #import <EZToolKit/UIViewController+EZ_Helper.h>
 #import <EZToolKit/UIColor+EZ_Helper.h>
 #import <EZToolKit/UIImage+EZ_Orientation.h>
+#import <EZToolKit/UIImage+EZ_Helper.h>
 #import <EZToolKit/UILabel+EZ_Rect.h>
 #import <EZToolKit/UIView+EZ_Helper.h>
 #import <EZToolKit/UIView+EZ_Action.h>
 #import <EZToolKit/UIView+EZ_Rect.h>
-
+#import <EZToolKit/UIWindow+EZ_Helper.h>
 //! Project version number for EZToolKit.
 FOUNDATION_EXPORT double EZToolKitVersionNumber;
 
