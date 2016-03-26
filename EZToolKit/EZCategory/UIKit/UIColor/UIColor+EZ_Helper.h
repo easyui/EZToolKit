1ab8089e485bf9ec52ff40aa66790507ce672580
easyui <gzhuyangjun@gmail.com>
2016-03-27 01:07:49 +0800

    add methods in UIColor+EZ_Helper

--- EZToolKit/EZCategory/UIKit/UIColor/UIColor+EZ_Helper.h
+++ EZToolKit/EZCategory/UIKit/UIColor/UIColor+EZ_Helper.h
@@ -7,15 +7,49 @@
 //
 
 #import <UIKit/UIKit.h>
+NS_ASSUME_NONNULL_BEGIN
 
 @interface UIColor (EZ_Helper)
-
+#pragma mark - Create a UIColor Object
 + (UIColor *)ez_randomColor;
 
 //ez_UIColorWithRGBHex:0x0298cf
 + (UIColor *)ez_UIColorWithRGBHex:(UInt32)hex;
++ (UIColor *)ez_UIColorWithRGBHex:(UInt32)hex alpha:(CGFloat)alpha;
+
 
 + (UIColor *)ez_UIColorWithRed:(CGFloat)red green:(CGFloat)green blue:(CGFloat)blue;
-
 + (UIColor *)ez_UIColorWithRed:(CGFloat)red green:(CGFloat)green blue:(CGFloat)blue alpha:(CGFloat)alpha;
+
+// @"0xF0F", @"66ccff", @"#66CCFF88"
++ (nullable UIColor *)colorWithHexString:(NSString *)hexStr;
+
+#pragma mark - Get color's description
+
+// 0x66ccff.
+- (uint32_t)ez_RGBHexValue;
+
+//0x66ccffff.
+- (uint32_t)ez_RGBAHexValue;
+
+//@"0066cc".
+- (nullable NSString *)ez_RGBhexString;
+
+// @"0066ccff".
+- (nullable NSString *)ez_RGBAhexString;
+
+
+//0.0 - 1.0
+@property (nonatomic, readonly) CGFloat ez_red;
+
+//0.0 - 1.0
+@property (nonatomic, readonly) CGFloat ez_green;
+
+//0.0 - 1.0
+@property (nonatomic, readonly) CGFloat ez_blue;
+
+//0.0 - 1.0
+@property (nonatomic, readonly) CGFloat ez_alpha;
+
 @end
+NS_ASSUME_NONNULL_END
--- EZToolKit/EZCategory/UIKit/UIColor/UIColor+EZ_Helper.m
+++ EZToolKit/EZCategory/UIKit/UIColor/UIColor+EZ_Helper.m
@@ -7,6 +7,7 @@
 //
 
 #import "UIColor+EZ_Helper.h"
+#import "NSString+EZ_Helper.h"
 
 @implementation UIColor (EZ_Helper)
 
@@ -19,6 +20,10 @@
 
 //ez_UIColorWithRGBHex:0x0298cf
 + (UIColor *)ez_UIColorWithRGBHex:(UInt32)hex {
+    return [UIColor ez_UIColorWithRGBHex:hex alpha:1.0];
+}
+
++ (UIColor *)ez_UIColorWithRGBHex:(UInt32)hex alpha:(CGFloat)alpha{
     int r = (hex >> 16) & 0xFF;
     int g = (hex >> 8) & 0xFF;
     int b = (hex) & 0xFF;
@@ -26,8 +31,9 @@
     return [UIColor colorWithRed:r / 255.0f
                            green:g / 255.0f
                             blue:b / 255.0f
-                           alpha:1.0f];
+                           alpha:alpha];
 }
+
 
 
 + (UIColor *)ez_UIColorWithRed:(CGFloat)red green:(CGFloat)green blue:(CGFloat)blue{
@@ -36,5 +42,135 @@
 
 + (UIColor *)ez_UIColorWithRed:(CGFloat)red green:(CGFloat)green blue:(CGFloat)blue alpha:(CGFloat)alpha{
     return [UIColor colorWithRed:red/255. green:green/255. blue:blue/255. alpha:alpha];
+}
+
++ (nullable UIColor *)colorWithHexString:(NSString *)hexStr{
+    CGFloat r, g, b, a;
+    if (hexStrToRGBA(hexStr, &r, &g, &b, &a)) {
+        return [UIColor colorWithRed:r green:g blue:b alpha:a];
+    }
+    return nil;
+}
+
+
+
+// 0x66ccff.
+- (uint32_t)ez_RGBHexValue{
+    CGFloat r = 0, g = 0, b = 0, a = 0;
+    [self getRed:&r green:&g blue:&b alpha:&a];
+    int8_t red = r * 255;
+    uint8_t green = g * 255;
+    uint8_t blue = b * 255;
+    return (red << 16) + (green << 8) + blue;
+}
+
+//0x66ccffff.
+- (uint32_t)ez_RGBAHexValue{
+    CGFloat r = 0, g = 0, b = 0, a = 0;
+    [self getRed:&r green:&g blue:&b alpha:&a];
+    int8_t red = r * 255;
+    uint8_t green = g * 255;
+    uint8_t blue = b * 255;
+    uint8_t alpha = a * 255;
+    return (red << 24) + (green << 16) + (blue << 8) + alpha;
+}
+
+//@"0066cc".
+- (nullable NSString *)ez_RGBhexString{
+    return [self hexStringWithAlpha:NO];
+}
+
+// @"0066ccff".
+- (nullable NSString *)ez_RGBAhexString{
+    return [self hexStringWithAlpha:YES];
+}
+
+
+
+- (CGFloat)ez_red {
+    CGFloat r = 0, g, b, a;
+    [self getRed:&r green:&g blue:&b alpha:&a];
+    return r;
+}
+
+- (CGFloat)ez_green {
+    CGFloat r, g = 0, b, a;
+    [self getRed:&r green:&g blue:&b alpha:&a];
+    return g;
+}
+
+- (CGFloat)ez_blue {
+    CGFloat r, g, b = 0, a;
+    [self getRed:&r green:&g blue:&b alpha:&a];
+    return b;
+}
+
+- (CGFloat)ez_alpha {
+    return CGColorGetAlpha(self.CGColor);
+}
+
+#pragma mark - private
+
+- (NSString *)hexStringWithAlpha:(BOOL)withAlpha {
+    CGColorRef color = self.CGColor;
+    size_t count = CGColorGetNumberOfComponents(color);
+    const CGFloat *components = CGColorGetComponents(color);
+    static NSString *stringFormat = @"%02x%02x%02x";
+    NSString *hex = nil;
+    if (count == 2) {
+        NSUInteger white = (NSUInteger)(components[0] * 255.0f);
+        hex = [NSString stringWithFormat:stringFormat, white, white, white];
+    } else if (count == 4) {
+        hex = [NSString stringWithFormat:stringFormat,
+               (NSUInteger)(components[0] * 255.0f),
+               (NSUInteger)(components[1] * 255.0f),
+               (NSUInteger)(components[2] * 255.0f)];
+    }
+    
+    if (hex && withAlpha) {
+        hex = [hex stringByAppendingFormat:@"%02lx",
+               (unsigned long)(self.ez_alpha * 255.0 + 0.5)];
+    }
+    return hex;
+}
+
+
+static inline NSUInteger hexStrToInt(NSString *str) {
+    uint32_t result = 0;
+    sscanf([str UTF8String], "%X", &result);
+    return result;
+}
+
+
+static BOOL hexStrToRGBA(NSString *str,
+                         CGFloat *r, CGFloat *g, CGFloat *b, CGFloat *a) {
+    str = [[str ez_stringByTrim] uppercaseString];
+    if ([str hasPrefix:@"#"]) {
+        str = [str substringFromIndex:1];
+    } else if ([str hasPrefix:@"0X"]) {
+        str = [str substringFromIndex:2];
+    }
+    
+    NSUInteger length = [str length];
+    //         RGB            RGBA          RRGGBB        RRGGBBAA
+    if (length != 3 && length != 4 && length != 6 && length != 8) {
+        return NO;
+    }
+    
+    //RGB,RGBA,RRGGBB,RRGGBBAA
+    if (length < 5) {
+        *r = hexStrToInt([str substringWithRange:NSMakeRange(0, 1)]) / 255.0f;
+        *g = hexStrToInt([str substringWithRange:NSMakeRange(1, 1)]) / 255.0f;
+        *b = hexStrToInt([str substringWithRange:NSMakeRange(2, 1)]) / 255.0f;
+        if (length == 4)  *a = hexStrToInt([str substringWithRange:NSMakeRange(3, 1)]) / 255.0f;
+        else *a = 1;
+    } else {
+        *r = hexStrToInt([str substringWithRange:NSMakeRange(0, 2)]) / 255.0f;
+        *g = hexStrToInt([str substringWithRange:NSMakeRange(2, 2)]) / 255.0f;
+        *b = hexStrToInt([str substringWithRange:NSMakeRange(4, 2)]) / 255.0f;
+        if (length == 8) *a = hexStrToInt([str substringWithRange:NSMakeRange(6, 2)]) / 255.0f;
+        else *a = 1;
+    }
+    return YES;
 }
 @end
