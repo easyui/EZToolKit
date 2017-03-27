42264b7de363f341d95d0f44cf2ef59a3429ebe9
zhuyangjun <zhuyangjun@qiyi.com>
2017-03-27 16:31:43 +0800

    add demo

---
+++ EZToolKitExample/EZToolKitExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EZToolKitExample
+//
+//  Created by IQIYI on 2017/3/27.
+//  Copyright © 2017年 Zhu yangjun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EZToolKitExample/EZToolKitExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EZToolKitExample
+//
+//  Created by IQIYI on 2017/3/27.
+//  Copyright © 2017年 Zhu yangjun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
